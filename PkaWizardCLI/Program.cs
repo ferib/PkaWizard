f14b2f12973bf2eead2e27d8e150ebcf288534d8
= <lopke123@live.nl>
2020-11-08 20:14:29 +0100

    dir repack scanning

--- PkaWizardCLI/Program.cs
+++ PkaWizardCLI/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using PkaWizardCli;
 using PkaWizardCli.Pka;
 using PkaWizardCli.Patcher;
+using PkaWizardCli.Repacker;
 
 namespace PkaWizardCli
 {
@@ -13,32 +14,67 @@ namespace PkaWizardCli
     {
         static void Main(string[] args)
         {
+
+            PacketTracerRepacker repacker = new PacketTracerRepacker("N:\\Cisco Packet Tracer 7.2.1");
+            repacker.RepackDirectory(0x42);
+
+            string RepackDir = "";
             string OutputPath = "";
             string PacketTracerPath = "";
             byte XorKey = 0x00;
 
             for (int i = 0; i < args.Length; i++)
             {
-                if (i == 0)
-                    PacketTracerPath = args[i];
-
-                switch(args[i])
+                // Patching
+                if(args[0].ToLower() == "patch")
                 {
-                    case "-k":
-                        if (args.Length > i + 1)
-                            if (!byte.TryParse(args[i + 1], out XorKey))
-                                Console.WriteLine("Error, invalid XOR key.");
-                        break;
-                    case "-o":
-                        if (args.Length > i + 1)
-                            OutputPath = args[i + 1];
-                        break;
-                    case "-h":
-                        Help();
-                        break;
-                    default:
-                        break;
+                    if (i == 0)
+                        PacketTracerPath = args[i];
+
+                    switch (args[i])
+                    {
+                        case "-k":
+                            if (args.Length > i + 1)
+                                if (!byte.TryParse(args[i + 1], out XorKey))
+                                    Console.WriteLine("Error, invalid XOR key.");
+                            break;
+                        case "-o":
+                            if (args.Length > i + 1)
+                                OutputPath = args[i + 1];
+                            break;
+                        case "-h":
+                            Help();
+                            break;
+                        case "-r":
+                            Console.WriteLine("Error, Restore not yet added");
+                            break;
+                        default:
+                            break;
+                    }
                 }
+                else if(args[0].ToLower() == "repack")
+                {
+                    if (i == 0)
+                        RepackDir = args[i];
+
+                    switch (args[i])
+                    {
+                        case "-k":
+                            if (args.Length > i + 1)
+                                if (!byte.TryParse(args[i + 1], out XorKey))
+                                    Console.WriteLine("Error, invalid XOR key.");
+                            break;
+                        case "-h":
+                            Help();
+                            break;
+                        case "-r":
+                            Console.WriteLine("Error, Restore not yet added");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
                 Console.WriteLine($"[{i}] {args[i]}");
             }
 
@@ -55,6 +91,10 @@ namespace PkaWizardCli
                 else
                     Console.WriteLine("Patching failed!");
             }
+            else if (RepackDir != "")
+            {
+
+            }
             else
                 Help();
 
@@ -63,17 +103,22 @@ namespace PkaWizardCli
 
         static void Help()
         {
-            string HelpContent =    "PkaWizardCli Usage: file [-h] [-k] [-o] \n\r" +
+            string HelpContent =    "PkaWizardCli Usage: [patch | repack] [file | dir] [-h] [-r] [-k] [-o] \n\r" +
                                     "\n\r" +
                                     "Patches PacketTracer unpacking algorithm\n\r" +
                                     "\n\r" +
                                     "Positional arguments:\n\r" +
-                                    "\tfile\tPacket Tracer binary file path\n\r" +
+                                    "\tpatch\tPatching Packet Tracer\n\r" +
+                                    "\trepack\tRepacking files\n\r" +
+                                    "\n\r" +
+                                    "\tfile\tPacket Tracer binary file path (patch only)\n\r" +
+                                    "\tdir\tDirectory to scan for .pks, .pkd and repack (repack only)\n\r" +
                                     "\n\r" +
                                     "Optional arguments: \n\r" +
                                     "\t-h\tShows this help message\n\r" +
-                                    "\t-k\tNew XOR key (stage 1)\n\r" +
-                                    "\t-o\tOutput File\n\r" +
+                                    "\t-k\tXOR key (stage 1)\n\r" +
+                                    "\t-o\tOutput File (patch only)\n\r" +
+                                    "\t-r\tRestore patch/repack (unfinished)\n\r" +
                                     "\n\r";
 
             Console.WriteLine(HelpContent);
---
+++ PkaWizardCLI/Repacker/PacketTracerRepacker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PkaWizardCli.Repacker
+{
+    class PacketTracerRepacker
+    {
+        private string ScanDirectory;
+        private bool MakeBackup = true;
+        private List<string> Directorys;
+
+        public PacketTracerRepacker(string path)
+        {
+            if(Directory.Exists(path))
+                this.ScanDirectory = path;
+            this.Directorys = new List<string>();
+        }
+
+        public bool RepackDirectory(byte xorKey)
+        {
+            string[] fileExtension = { ".pka", ".pts", ".ptd" };
+            List<string> files = new List<string>();
+
+            // collect files
+            GetDirectorys(this.ScanDirectory);
+            foreach(var d in this.Directorys)
+            {
+                // check if file contains fileExtension
+                foreach (var f in Directory.GetFiles(d))
+                    foreach(var ext in fileExtension)
+                        if(f.Substring(f.Length-ext.Length, ext.Length) == ext)
+                        {
+                            files.Add(f);
+                            break;
+                        } 
+            }
+
+            // re-pack files
+            foreach(var file in files)
+            {
+                // backup
+                if(this.MakeBackup)
+                {
+                    if(!File.Exists(file + ".bak"))
+                    {
+                        // only backup if .bak of file not exists, or we may corrupt original backup files
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private void GetDirectorys(string path)
+        {
+            var dirs = Directory.GetDirectories(path);
+                this.Directorys.AddRange(dirs);
+                foreach (var d in dirs)
+                    GetDirectorys(d);
+        }
+    }
+}
